4ca174ef276a1486e3c98e002bb29696bf7fa388
dykatania <tanka.vachko@gmail.com>
2022-07-22 22:26:57 +0300

    homework 1 js

---
+++ homework1/main.js
@@ -0,0 +1,67 @@
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua'
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let pi = 3.14;
+let n5 = 2.7;
+let n6 = 16;
+let bt = true;
+let bf = false;
+
+alert(`${s1} ${s2} ${s3} ${s4}`);
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+document.write(`${s1} ${s2} ${s3} ${s4}`);
+
+alert(pi);
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+
+document.write(`${pi},${n1},${n2},${n3},${n4},${n5},${n6}`);
+
+alert(`${bt} ${bf}`);
+
+
+
+let firstName = 'Tetiana';
+let middleName = 'Romanivna';
+let lastName = 'Vachko';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+
+
+let name1 = prompt('What is your first name?',firstName);
+console.log(name1);
+
+let name2 = prompt('What is your middle name?',middleName);
+console.log(name2);
+
+let age = prompt('How old are you?',22);
+console.log(age);
